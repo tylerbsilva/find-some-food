159f06819d1927c10184e0fea08e4a343a3a0465
Tyler Silva <me@tylerbsilva.com>
2016-07-13 17:11:27 -0700

    Added geolocation and template for places

--- js/app.js
+++ js/app.js
@@ -1,18 +1,23 @@
-// API KEY: AIzaSyDRj8hLWs2TGSwdlB4eXTYF8EF6QwG-74M
-// GEOCODING LINK: https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyDZT3ioc8QQS0Z-vm6uOfJWf6sLkosI4oY
+"use strict";
+// HELPER FUNCTION THANKS TO:
+// http://jsforallof.us/2014/12/01/the-anatomy-of-a-simple-templating-engine/
+var Templater = function(html){
+  return function(data){
+    for(var x in data){
+      var re = "{{\\s?" + x + "\\s?}}";
+      html = html.replace(new RegExp(re, "ig"), data[x]);
+    }
+    return html;
+  };
+};
 
+// START OF APPLICATION
 function App(){
 	// SET DEFAULT LOCATION TO SF
 	var CURRENT_LOCATION = new google.maps.LatLng(37.7749, -122.4194);
 	var CURRENT_DATA, MAP;
 	// INITIALIZE FUNCTION, TAKES THE 3 IDS THAT WILL HANDLE INPUT AND OUTPUT
-	function init(formID, searchID, mapID, outputID){
-		//CHECK IF GEOLOCATION IS AVAILABLE
-		if ("geolocation" in navigator) {
-			navigator.geolocation.getCurrentPosition(function(position) {
-				CURRENT_LOCATION = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-			});
-		}
+	function init(formID, searchID, mapID, outputID, locationID){
 		setupMap(mapID);
 		// SETUP FORM SUBMISSION
 		document.getElementById(formID).addEventListener('submit', function(e){
@@ -24,51 +29,76 @@ function App(){
 				var outputEl = document.getElementById(outputID);
 				outputEl.innerHTML = ""
 				for(var x in data){
-					outputEl.appendChild(Place(data[x]).loadHTML());
+					outputEl.innerHTML += Place(data[x]).loadHTML();
 				}
 			});
 		});
-	};
+		// SETUP GEOLOCATION
+		document.getElementById(locationID).addEventListener('click', function(e){
+			e.preventDefault()
+			//CHECK IF GEOLOCATION IS AVAILABLE
+			if ("geolocation" in navigator) {
+				navigator.geolocation.getCurrentPosition(function(position) {
+					CURRENT_LOCATION = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+					SearchForm().handleSubmit(CURRENT_LOCATION, MAP, function(data, status, location){
+						CURRENT_DATA = data;
+						var outputEl = document.getElementById(outputID);
+						outputEl.innerHTML = ""
+						for(var x in data){
+							outputEl.innerHTML += Place(data[x]).loadHTML();
+						}
+					});
+				});
+			} else {
+				alert("Sorry, using geolocation isn't available in your browser");
+			}
+		});
+	}; // END INIT
 	function setupMap(mapID){
 		MAP = new google.maps.Map(document.getElementById(mapID), {
       center: new google.maps.LatLng(CURRENT_LOCATION.lat, CURRENT_LOCATION.lng),
       zoom: 15
     });
-	};
+	}; // END SETUP MAP
 	function getCurrentLocation(){
 		return CURRENT_LOCATION;
-	};
+	}; // END GET CURRENT LOCATION
 	function getCurrentData(){
 		return CURRENT_DATA;
-	};
-	/********** PLACE CONSTRUCTOR **********/
-	function Place(data){
-		if(!data) throw Error("No data for Place constructor");
-		var DATA = data;
+	}; // ENT GET CURRENT DATA
+	return {
+		init: init,
+		getCurrentLocation : getCurrentLocation,
+		getCurrentData : getCurrentData
+	}
+} // END APP
 
-		function getData(){
-			return DATA;
-		}; // END GET DATA
 
-		function loadHTML(){
-			var newEl = document.createElement('li');
-			newEl.className = "place"
-			newEl.innerHTML += "<h3>" + DATA.name + "</h3>";
-			newEl.innerHTML += "<p>LOCATION: " + DATA.vicinity + "</p>";
-			newEl.innerHTML += DATA.rating ? "<p>RATING: " + DATA.rating + "</p>" : "<p>RATING: N/A</p>"
-			return newEl;
-		}; // END LOAD HTML
-		return {
-			getData : getData,
-			loadHTML : loadHTML
-		}
-	}; // END PLACE CONSTRUCTOR
-	/********** SEARCH FORM CONSTRUCTOR **********/
-	function SearchForm(){
-		function handleSubmit(input, mapObj, callback){
-			var service = new google.maps.places.PlacesService(mapObj);
-			var geocoder = new google.maps.Geocoder();
-			var location;
+/********** PLACE CONSTRUCTOR **********/
+function Place(data){
+	if(!data) throw Error("No data for Place constructor");
+	var DATA = data;
+
+	function getData(){
+		return DATA;
+	}; // END GET DATA
+
+	function loadHTML(){
+		var template = Templater("<li class=\"place\"><h1>{{ name }}</h1><a href=\"https://www.google.com/maps/place/{{ vicinity }}\" target=\"_blank\">{{ vicinity }}</a><p>RATING: {{ rating }}</p></li>");
+		return template(DATA);
+	}; // END LOAD HTML
+	return {
+		getData : getData,
+		loadHTML : loadHTML
+	}
+}; // END PLACE CONSTRUCTOR
+/********** SEARCH FORM CONSTRUCTOR **********/
+function SearchForm(){
+	function handleSubmit(input, mapObj, callback){
+		var service = new google.maps.places.PlacesService(mapObj);
+		var geocoder = new google.maps.Geocoder();
+		var location;
+		if(typeof input === "string"){
 			geocoder.geocode({
 				address: input
 			}, function(data, status){
@@ -78,34 +108,35 @@ function App(){
 				location = data[0].geometry.location;
 				service.nearbySearch({
 					location: location,
-					radius: '5000',
-					types: ['bakery', 'cafe', 'restaraunt', 'meal delivery']
+					radius: '2000',
+					type: ['restaurant']
 				}, function(data, status){
 					callback(data, status, location);
 				});
 			});
-		}; // END HANDLE SUBMIT
-		function geocodeAddress(addressString, callback){
-			geocoder.geocode({
-				address : addressString
+		} else {
+			service.nearbySearch({
+				location: input,
+				radius: '2000',
+				type: ['restaurant']
 			}, function(data, status){
-				callback(data,status);
+				callback(data, status, location);
 			});
-		} // END GEOCODE ADDRESS
-		return {
-			handleSubmit : handleSubmit
-		};
-	}; // END SEARCH FORM CONSTRUCTOR
-	/********** RETURN NECESSARY APP CONTENTS **********/
+		}
+	}; // END HANDLE SUBMIT
+	function geocodeAddress(addressString, callback){
+		geocoder.geocode({
+			address : addressString
+		}, function(data, status){
+			callback(data,status);
+		});
+	} // END GEOCODE ADDRESS
 	return {
-		init: init,
-		getCurrentLocation : getCurrentLocation,
-		getCurrentData : getCurrentData,
-		Place : Place,
-		SearchForm : SearchForm
-	}
-}
+		handleSubmit : handleSubmit
+	};
+}; // END SEARCH FORM CONSTRUCTOR
+
 
 
 var app = App();
-app.init('placesForm', 'placesSearch', 'mapHolder', 'placesOutput');
+app.init('placesForm', 'placesSearch', 'mapHolder', 'placesOutput', 'useLocation');
